28498717698853e8f74689f0092fe4da7c8cad80
Kaleb Lape <kaleb.lape@metova.com>
2017-09-25 10:31:09 -0500

    Add Fold.

---
+++ test/unit/fold/react.test.js
@@ -0,0 +1,20 @@
+import test, { test as it } from 'ava'
+import Fold from '../../../lib/fold'
+
+let create = (...params) => new Fold(...params)
+
+test('returns reactions from neurons', t => {
+  let reactions = [1, 2]
+  let neurons = reactions.map(reaction => ({ react: () => reaction }))
+  let subject = create(neurons)
+
+  t.deepEqual(subject.react(), reactions)
+})
+
+test('passes inputs to neurons', t => {
+  let neurons = [{ react: (inputs) => inputs }]
+  let subject = create(neurons)
+  let inputs = [1, 2, 3]
+
+  t.deepEqual(subject.react(inputs), [inputs])
+})
