af454ab4d2860bbc8de8d0df782c0c946e2b8a2a
Kaleb Lape <kaleb.lape@metova.com>
2017-10-20 11:33:23 -0500

    Clean up test playground.

--- test/playground.test.js
+++ test/playground.test.js
@@ -14,10 +14,6 @@ test(t => {
     .outputs(3)
     .build()
 
-  t.log(`input: ${input}`)
-  t.log(`output: ${brain.react(input)}`)
-
-  t.log(`answer: ${new SigmoidActivator().activate(1.547)}`)
   t.truthy(brain)
 })
 
@@ -26,69 +22,54 @@ test(t => {
     .inputs(2)
     .layer(2)
     .layer(2)
-    // .layer(2)
-    // .depth(3)
-    // .breadth(3)
     .outputs(1)
     .build()
 
-  const andData = [
-    [[0, 0, 1], [0]],
-    [[0, 1, 1], [0]],
-    [[1, 0, 1], [0]],
-    [[1, 1, 1], [1]],
-  ]
+  // const andData = [
+  //   [[0, 0, 1], [0]],
+  //   [[0, 1, 1], [0]],
+  //   [[1, 0, 1], [0]],
+  //   [[1, 1, 1], [1]],
+  // ]
 
-  const xorData = [
-    [[0, 0], [0]],
-    [[1, 0], [1]],
-    [[0, 1], [1]],
-    [[1, 1], [0]],
-  ]
+  // const xorData = [
+  //   [[0, 0], [0]],
+  //   [[1, 0], [1]],
+  //   [[0, 1], [1]],
+  //   [[1, 1], [0]],
+  // ]
 
-  const lineData = _.chunk(_.times(10000, () => Math.random() * 10), 2).map(([x, y]) => {
+  const lineData = _.chunk(_.times(100, () => Math.random() * 10), 2).map(([x, y]) => {
     return [[x,y], [y > x ? 1 : 0]]
   })
 
-  // Examples:
-  // [
-  //   [[1, 2], [1]],
-  //   [[1, 0], [0]],
-  //   [[2, 2.5], [1]],
-  //   [[1, 1.5], [1]],
-  //   [[3, 1], [0]],
-  //   [[4, 1], [0]],
-  //   [[5, 1], [0]],
-  // ]
-
-  const trainingSet = xorData
+  const trainingSet = lineData
 
   const trainer = new Trainer(brain, { learningRate: .1 })
 
-  t.log('Before training...')
-  // console.log(JSON.stringify(brain, null, 2))
-  trainingSet.forEach(([input, output]) => {
+  const beforeError = trainingSet.reduce((error, [input, output]) => {
     const reaction = brain.react(input)
-    t.log(`react(${input}) -> ${reaction}`)
+
+    let errors = _.zipWith(reaction, output, _.subtract).map(Math.abs)
+    if (_.isNumber(error)) errors = errors.concat(error)
+
+    return _.mean(errors)
   })
 
-  t.log('Training...')
-  _.times(50000, () => {
+  _.times(100, () => {
     trainingSet.forEach(([input, output]) => {
       trainer.train(input, output)
     })
-
-    // console.log(JSON.stringify(brain, null, 2))
-
   })
-  t.log('After training...')
-  trainingSet.forEach(([input, output]) => {
+
+  const afterError = trainingSet.reduce((error, [input, output]) => {
     const reaction = brain.react(input)
-    t.log(`react(${input}) -> ${reaction}`)
+
+    let errors = _.zipWith(reaction, output, _.subtract).map(Math.abs)
+    if (_.isNumber(error)) errors = errors.concat(error)
+
+    return _.mean(errors)
   })
 
-  // t.true(brain.react([0, 0, 1])[0] < 0.5)
-  // t.true(brain.react([0, 1, 1])[0] < 0.5)
-  // t.true(brain.react([0, 0, 1])[0] < 0.5)
-  // t.true(brain.react([1, 1, 1])[0] >= 0.5)
+  t.true(beforeError > afterError)
 })
