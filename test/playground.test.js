88d03480e2e3994ba9a5225ad4794ace425c0dd9
Kaleb Lape <kaleb.lape@metova.com>
2017-11-13 15:19:07 -0600

    Add failing mnist handwriting test.

--- test/playground.test.js
+++ test/playground.test.js
@@ -3,6 +3,7 @@ import _ from 'lodash'
 import BrainBuilder from '../lib/brain-builder'
 import Trainer from '../lib/trainer'
 import SigmoidActivator from '../lib/activators/sigmoid'
+import mnist from 'mnist'
 
 test(t => {
   const size = 10
@@ -71,4 +72,46 @@ test(t => {
   })
 
   t.true(beforeError > afterError)
+})
+
+test(t => {
+  const size = 28 * 28
+  const brain = BrainBuilder.new(new SigmoidActivator)
+    .inputs(size)
+    .layer(15)
+    .outputs(10)
+    .build()
+
+  function selected(output) {
+    return output.indexOf(_.max(output))
+  }
+
+  const trainer = new Trainer(brain, { learningRate: 1 })
+
+  const totalSessions = 30
+  const trainingCount = 2000
+  const testCount = 10000
+  const set = mnist.set(trainingCount, testCount)
+  _.times(30, (sessionNumber) => {
+    const set = mnist.set(trainingCount, testCount)
+    console.log(`Session ${sessionNumber + 1}/${totalSessions}...`)
+    set.training.forEach(({ input, output }, index) => {
+      trainer.train(input, output)
+    })
+
+    const successes = set.test
+      .map(({ input, output }) => selected(brain.react(input)) == selected(output))
+      .filter(_.identity)
+      .length
+    console.log(`  Session complete: ${successes}/${testCount} successes...`)
+  })
+
+  const results = brain.react(set.test[0].input)
+
+  set.test.forEach(({ input, output }) => {
+    const result = brain.react(input)
+  })
+
+  t.is(selected(brain.react(set.training[0].input)), selected(set.training[0].output))
+  t.is(selected(results), selected(set.test[0].output))
 })
