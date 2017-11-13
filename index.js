49eebc3c2d77fbfc4e7e7e8aacb79261783ad923
Kaleb Lape <kaleb.lape@metova.com>
2017-11-13 15:27:21 -0600

    Export modules.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const Neuron = require('./lib/neuron')
+const Brain = require('./lib/brain')
+const Fold = require('./lib/fold')
+const BrainBuilder = require('./lib/brain-builder')
+const Trainer = require('./lib/trainer')
 
-module.exports = { Neuron }
+const Activators = {}
+Activators.Sigmoid = require('./lib/activators/sigmoid')
+
+module.exports = { Activators, Neuron, Brain, Fold, BrainBuilder, Trainer }
